736f65337846ae0f7fa93fb152fe98876df335c2
aprimMestir <fatmestir14381@gmail.com>
2026-09-04 13:06:45 +0300

    Add custom Ability System Component

---
+++ Source/skill/Abilities/SkillAbilitySystemComponent.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "SkillAbilitySystemComponent.generated.h"
+
+UCLASS(ClassGroup=(Abilities), meta=(BlueprintSpawnableComponent))
+class SKILL_API USkillAbilitySystemComponent : public UAbilitySystemComponent
+{
+    GENERATED_BODY()
+};

d4604a080081654ad55c85b2e8eb3c38e29b9698
aprimMestir <fatmestir14381@gmail.com>
2026-09-04 13:13:45 +0300

    Add tag-based ability activation helper

--- Source/skill/Abilities/SkillAbilitySystemComponent.h
+++ Source/skill/Abilities/SkillAbilitySystemComponent.h
@@ -2,10 +2,15 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayTagContainer.h"
 #include "SkillAbilitySystemComponent.generated.h"
 
 UCLASS(ClassGroup=(Abilities), meta=(BlueprintSpawnableComponent))
 class SKILL_API USkillAbilitySystemComponent : public UAbilitySystemComponent
 {
     GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category="Abilities")
+    bool TryActivateAbilityByTag(FGameplayTag AbilityTag);
 };
